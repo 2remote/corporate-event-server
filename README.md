faeb41ee89cfafff01ce559ad8517179fa0f1340
Hu Songtao <idisblueflash@gmail.com>
2016-04-28 20:42:15 +0800

    获取文件信息

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 ## 配置
 1. 根目录下创建development.json文件，放入端口号和AccessKey/SecretKey。
+2. 访问文件详情 curl http://localhost:8080/qiniu/businesssync/livepictest426/4.jpg
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var async = require('async');
 var ejs = require('ejs');
 var fs = require('fs');
 var conf = require('./cfg');
+var qiniu = require('qiniu');
 
 var app = express();
 app.set('views', __dirname + '/views');
@@ -33,10 +34,29 @@ var photos = [
 	},
 ];
 
+//init qiniu
+qiniu.conf.ACCESS_KEY = conf('ACCESS_KEY');
+qiniu.conf.SECRET_KEY = conf('SECRET_KEY');
+
+var client = new qiniu.rs.Client();
+
 // homepage by event name
 app.get('/:event_name', function(req, res){
   var event_name = req.params.event_name;
   res.render('event', {title: event_name, photos: photos});
+});
+
+app.get('/qiniu/:bucket/:event/:filename', function(req, res){
+	var bucket = req.params.bucket;
+	var event = req.params.event;
+	var filename = req.params.filename;
+  var key = event + "/" + filename;
+	client.stat(bucket, key, function(err, ret) {
+		if (err) {
+      console.error(err);
+    };
+		res.json(ret);
+	});
 });
 
 var port = conf('PORT');
