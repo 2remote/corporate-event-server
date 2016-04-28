71e087365fa4dba12b6a891cdda3d146e2fd5540
Hu Songtao <idisblueflash@gmail.com>
2016-04-28 23:59:38 +0800

    fix: 样式错乱

--- app.js
+++ app.js
@@ -12,28 +12,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use('/', express.static(__dirname + '/public'));
 
-
-var photos = [
-	{
-		bigImage: 'images/rice.jpg',
-		title: 'rice',
-		thumbnail: 'images/thumbnail/rice.jpg',
-		alt:'rice'
-	},
-	{
-		bigImage: 'images/nuddles.jpg',
-		title: 'nuddles',
-		thumbnail: 'images/thumbnail/nuddles.jpg',
-		alt:'nuddles'
-	},
-	{
-		bigImage: 'images/mushroom.jpg',
-		title: 'mushroom',
-		thumbnail: 'images/thumbnail/mushroom.jpg',
-		alt:'mushroom'
-	},
-];
-
 //init qiniu
 qiniu.conf.ACCESS_KEY = conf('ACCESS_KEY');
 qiniu.conf.SECRET_KEY = conf('SECRET_KEY');
@@ -41,38 +19,22 @@ qiniu.conf.SECRET_KEY = conf('SECRET_KEY');
 var client = new qiniu.rs.Client();
 var listPhotos= qiniu.rsf.listPrefix;
 var corporatePreLink = 'http://7xtehn.com2.z0.glb.qiniucdn.com/';
+var bucket = 'businesssync';
 
-// homepage by event name
-app.get('/:event_name', function(req, res){
-  var event_name = req.params.event_name;
-  res.render('event', {title: event_name, photos: photos});
-});
-
-app.get('/qiniu/:bucket/:event/:filename', function(req, res){
-	var bucket = req.params.bucket;
-	var event = req.params.event;
-	var filename = req.params.filename;
-  var key = event + "/" + filename;
-	client.stat(bucket, key, function(err, ret) {
-		if (err) {
-      console.error(err);
-    };
-		res.json(ret);
-	});
-});
-
-app.get('/qiniu/:bucket/:event', function(req, res){
-	var bucket = req.params.bucket;
+// get gallery by event name
+app.get('/:event', function(req, res){
 	var event_name = req.params.event;
 	listPhotos(bucket, event_name, false, 10, false,  function(err, ret) {
-		if (err) {
-      console.error(err);
-    };
     var images = [];
-    ret.items.forEach(function(img){
-      images.push(covertImageInfo(img.key, img.hash, img.mimType, img.putTime));
-    });
-    res.render('event', {title: event_name, photos: images});
+		if (err) {
+     console.error('[ERROR]', err);
+    }else{
+      console.log('ret', ret);
+      ret.items.forEach(function(img){
+        images.push(covertImageInfo(img.key, img.hash, img.mimType, img.putTime));
+        });
+      res.render('event', {title: event_name, photos: images});
+    }
 	});
 });
 
@@ -88,9 +50,10 @@ function covertImageInfo(key, hash, mimType, putTime){
   var org = corporatePreLink + key;
   var bigImage = org + '?imageMogr2/thumbnail/1024x';
   var thumbnail = org + '?imageMogr2/thumbnail/300x';
+  var title = key.split('/')[1];
   return { 
     bigImage: bigImage, 
-    title: key, 
+    title: title, 
     thumbnail: thumbnail, 
     alt: key, 
   };
