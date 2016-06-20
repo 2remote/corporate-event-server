780b0dba9431bb29008a7603a13fe1efaf6b90e7
Hu Songtao <idisblueflash@gmail.com>
2016-06-20 12:39:20 +0800

    feat: new filename format support
    yyyymmddhhMMss-文件名.jpg

--- test/03_yaopaiPhotoNameHandle.local.js
+++ test/03_yaopaiPhotoNameHandle.local.js
@@ -3,7 +3,7 @@ var common = require('../lib/common.js');
 
 var mmexportKey = 'mmexport1464827976760.jpg';
 var normalKey = '1464827976760.jpg';
-
+var newDateKey= '20160618175714-9T4A7899.jpg';
 
 test('YAOPAI photo name handle', function(t) {
   var dateTime = common.yaopaiPhotoNameHandle(normalKey);
@@ -13,6 +13,10 @@ test('YAOPAI photo name handle', function(t) {
   dateTime = common.yaopaiPhotoNameHandle(mmexportKey);
   t.equal(typeof dateTime, 'number', 'a stirng');
   t.equal(dateTime, 1464827976760, 'right number');
+
+  dateTime = common.yaopaiPhotoNameHandle(newDateKey);
+  t.equal(typeof dateTime, 'number', 'a stirng');
+  t.equal(dateTime, 1466243834000, 'right number');
 
   t.end();
 });
