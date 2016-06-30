705a4e5f53e3013b941cd6670510a26e8c29f8fc
Hu Songtao <idisblueflash@gmail.com>
2016-06-30 23:07:37 +0800

    add bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,13 @@
 module.exports = function(grunt) {
   'use strict';
   grunt.initConfig({
+    bump: {
+      options: {
+        commit: false,
+        createTag: true,
+        push: false
+      }
+    },
     jshint: {
       debug: [
         'test/**/*.js',
@@ -23,8 +30,10 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-tape');
+  grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('test', ['jshint', 'tape']);
+  grunt.registerTask('default', ['jshint:debug']);
+  grunt.registerTask('test', ['jshint', 'tape', 'bump']);
 };
