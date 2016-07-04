c2fa10124a97124125b7713b9f1e0c608c7d3722
Hu Songtao <idisblueflash@gmail.com>
2016-07-04 16:11:35 +0800

    添加原功能测试

--- test/04_remove_date_watermark_on_banner.js
+++ test/04_remove_date_watermark_on_banner.js
@@ -29,6 +29,21 @@ test('format', function(t) {
     timeFormat: '拍照时间',
     title: '12月20日 12:20:20'
   };
-  t.deepEqual(common.covertImageInfo(...sourceObj), targetObj, 'convert right shot time');
+  t.deepEqual(common.covertImageInfo(...sourceObj), targetObj, 'show shot time when filename contains right shot time format');
+
+  var badShotTimeFilename = '2016620test/fox-love-all.png';
+  var sourceObjBad = [badShotTimeFilename, 'Ft8djAzHxRgwOK6xano0CBklBDp5', 'image/jpeg', 14663939041780276, '2016620test'];
+  targetObj = {
+    alt: '2016620test/fox-love-all.png',
+    bigImage: 'undefined2016620test/fox-love-all.png-m2016620test',
+    putDate: '6月20日 11:38:24',
+    putTime: 14663939041780276,
+    shotTime: false,
+    thumbnail: 'undefined2016620test/fox-love-all.png-s',
+    timeFormat: '上传时间',
+    title: '6月20日 11:38:24'
+  };
+  t.deepEqual(common.covertImageInfo(...sourceObjBad), targetObj, 'show put time when filename contains wrong shot time format');
+
   t.end();
 });
