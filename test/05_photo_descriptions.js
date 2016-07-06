a19d35776edd5c5d053cf7ddbe1822dfac5a3e83
Hu Songtao <idisblueflash@gmail.com>
2016-07-06 10:23:29 +0800

    feat: 大图显示原型

---
+++ test/05_photo_descriptions.js
@@ -0,0 +1,10 @@
+var test = require('tape');
+var proxyquire = require('proxyquire');
+var sinon = require('sinon');
+var common = require('../lib/common.js');
+
+
+test('check photo description', function(t) {
+  t.ok(true, 'init');
+  t.end();
+});
